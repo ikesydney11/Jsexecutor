2b18e358f12386b63887091a19780ec6864e3223
iaghanchi <ikesydney11@gmail.com>
2025-05-27 13:12:38 +0100

    Playing with playstack

---
+++ src/test/java/JsExecutor.java
@@ -0,0 +1,45 @@
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class JsExecutor {
+    static ExtentReports extent = new ExtentReports();
+    static ExtentSparkReporter spark = new ExtentSparkReporter("target/spark.html");
+     @Test
+    public static void setUp() {
+        extent.attachReporter(spark);
+        extent.setSystemInfo("Author", "Ikechukwu");
+
+        ExtentTest test = extent.createTest("Automation Tab Test")
+                .assignCategory("Smoke Test")
+                .addScreenCaptureFromBase64String("Img", "My screenshots")
+                .assignDevice("Firefox");
+        WebDriver driver = new FirefoxDriver();
+        test.info("Browser started");
+        try {
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+            driver.get("https://www.browserstack.com");
+            test.info("Navigated to browserstack.com");
+            WebElement automation = driver.findElement(By.id("menu-item-91794"));
+            automation.click();
+            test.pass("Clicked on Automation menu item");
+
+        } catch (Exception e) {
+            test.fail("Test failed: " + e.getMessage());
+        } finally {
+            driver.quit();
+            test.info("Browser closed");
+            extent.flush();
+        }
+    }
+
+
+}
